fa7aa60382d3759a8a8f39bd030bb5e9117b6dc9
Faeka <faeka6@gmail.com>
2022-03-06 22:07:57 +0530

    Starting a Complete Web development Serie

---
+++ 1_introduction.c
@@ -0,0 +1,72 @@
+/*
+WEB DEVELOPMENT?
+How web works?
+HTML CSS JS - basic web technologies 
+Frontend
+Backend
+
+Frontend:
+HTML
+CSS
+JS
+FRAMEWORKS
+
+Backend:
+Version control- GIT/ Github / open source
+Databases- SQL
+
+HTML- skeleton/Body
+CSS- Beauty
+Javascript- Brain
+
+_____________________________________________________________________________
+
+Introduction to HTML, CSS, JavaScript & How websites work? | Web Development
+I started this course as an attempt to teach you web development in a fun, easy way! 
+The aim of this course is to teach you how to build a high-quality, responsive, and modern website 
+using HTML, CSS, and JavaScript.
+
+In this course, you will learn about all the technologies involved in web development. 
+This course is going to benefit all those 
+students who do not know anything and are new to this field. You will get complete knowledge of HTML, CSS, 
+and JavaScript. You will get full information from scratch about web development. 
+Also, those who already have some knowledge can go through the course to learn even further 
+as there are a lot of advanced websites that we will look into by the end of this course.
+
+Now let us first understand what HTML, CSS, and Javascript are for? 
+For every website to be designed, HTML (HyperText Markup Language) is a must. 
+This is the skeleton of a website. Without it, no website can run. 
+CSS (Cascading Style Sheets) adds beauty to that website and 
+JavaScript adds the brain to allow the functioning of that website. 
+Therefore, CSS and JavaScript add beauty and brain to a particular website respectively. 
+
+Let us now take another example of a car. The HTML acts as the metallic body of a car and the 
+CSS acts as the color and design of the car. Finally, the engine of a car is like the 
+JavaScript on the website to add functionality. 
+
+Normally, a client or a user sends a request to the webserver of the website,he wants to visit. 
+The web server that has its own IP address stores all the files in the backend which can be written in PHP,
+Python, or Node.js. The web server sends a response to the client in the form of HTML, CSS, and JavaScript. 
+
+Finally, to understand-
+#HTML- skeleton/body- is used as a standard language for any website design. It acts as a static skeleton to a web application. It’s a well-standardized system.
+#CSS- looks/parts- is used to handle the presentation of the web page. It makes the website look attractive and beautiful.
+#JavaScript- functions/brain- allows scripting on your website and makes it completely dynamic in nature. It provides front end scripting for your website and is a high-level dynamic interpreted programming language.
+
+Now, HTML, CSS, and JavaScript have a lot of concepts in them which we will take over through 
+the span of this course. Therefore, it cannot be learned all at once. If you are a beginner, 
+learning HTML up to 80%, CSS up to 40-60%, and JavaScript up to 50-70% will be more than enough 
+to start building websites. This is something nobody in the web development industry talks about and 
+hence I thought to share this personal advice with all of you.  Please don't get me wrong when I say that.
+Once you have learned and understood the bare minimum to build a website you can come back to the course
+ and start watching it once again for perfecting and sharpening the already learned concepts. 
+ Sometimes following this practice gets difficult, the good news is that once you start watching 
+ these videos and get your hands dirty with basic HTML, CSS, and JavaScript, 
+ the rest you will gradually learn based upon your practices and experiences with the help of projects 
+ I have designed as a part of this. You need to learn all the concepts simultaneously and co-relate all 
+ the things by practicing them.
+
+
+
+
+*/
